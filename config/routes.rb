5080bcafc5585236e8ff41aaf055dbf98e43069d
Jennifer <jennifersoden@hotmail.com>
2016-06-05 15:39:53 -0600

    Add styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
+  namespace :users do
+    resources :favorites, only: [:show]
+  end
+
   namespace :admin do
     resources :categories, only: [:new, :create]
     resources :gifs,       only: [:new, :create]
